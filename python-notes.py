84972aeaabf757d4450f58262d3227cc3c13bc32
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-02-22 23:59:22 -0600

    Learned a little bit about the modulus operator

--- python-notes.py
+++ python-notes.py
@@ -325,5 +325,16 @@ print('Float Division:', float_division) # Float Division: 2.222222222222222
 # If you add an integer and a float, the result is automatically converted to a float: 
 my_int = 56
 my_float = 5.4
-sum_int_and_float = my_int + my_float
-print(sum_int_and_float)
+sum_int_and_float = my_int + my_float # 61.4
+print(sum_int_and_float) # <class 'float'>
+    # This is true for other arithmetic operations like subtraction, multiplication, and division. Always a float reult. 
+
+# Modulo operator: returns the remainder when the value on the left is divided by the value on the right, and it is represented by %
+my_int_1 = 56
+my_int_2 = 12
+my_float_1 = 5.4
+my_float_2 = 12.0
+mod_ints = my_int_1 % my_int_2
+mod_floats = my_float_2 % my_float_1
+print('Integer Modulus:', mod_ints) # Integer Modulus: 8
+print('Float Modulus:', mod_floats) # Float Modulus: 1.1999999999999993
