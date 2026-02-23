3cb4f39bd1b57975349606778746310a46232b41
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-02-23 14:13:23 -0600

    finished objective 5: How to ork with integers and floating point numbers in python

--- python-notes.py
+++ python-notes.py
@@ -338,3 +338,61 @@ mod_ints = my_int_1 % my_int_2
 mod_floats = my_float_2 % my_float_1
 print('Integer Modulus:', mod_ints) # Integer Modulus: 8
 print('Float Modulus:', mod_floats) # Float Modulus: 1.1999999999999993
+
+# Floor division divides two numbers and returns the greatest integer less than or equal to the result, done with //
+my_int_1 = 56
+my_int_2 = 12
+my_float_1 = 5.4
+my_float_2 = 12.0
+floor_div_ints = my_int_1 // my_int_2
+floor_div_floats = my_float_2 // my_float_1
+print('Integer Floor Division:', floor_div_ints) #Integer Floor Division: 4, because 56/12 = 4.6666666667
+print('Float Floor Division:', floor_div_floats) # Float Floor Division: 2.0, because 12/5.4 = 2.2222222222
+
+# Exponentiation: Raises a number to the power of antother, and is done with **
+my_int_1 = 56
+my_int_2 = 12
+my_float_1 = 5.4
+my_float_2 = 12.0
+exp_ints = my_int_1 ** my_int_2
+exp_floats = my_float_1 ** my_float_2
+print('integer Exponentiation:', exp_ints) # Integer Exponentiation: 951166013805414055936
+print('Float Exponentiation;', exp_floats) # Float Exponentiation: 614787626.1765089
+
+# float() : Function that returns a foationg-point number constructed from the givern number (convreting either numberic data or strings into integer or floats)
+my_int_1 = 56
+my_float_1 = float(my_int_1)
+print(my_float_1) # 56.0
+print(type(my_float_1)) # <class 'float>
+
+# int() : function that returns an integer constructed from the given number: 
+my_float = 12.92563
+my_int = int(my_float)
+print(my_int) # 12
+print(type(my_int)) # <class 'int'>
+    # You can use the same logic to convert a stirng into either a float or an integer
+my_str_int = '45'
+my_str_float = '7.8'
+converted_int = int(my_str_int)
+converted_float = float(my_str_float)
+print(converted_int, type(converted_int)) # 45 <class 'int'>
+print(converted_float, type(converted_float)) # 7.8 <class 'float'>
+
+# round() : Rounds a number to the specified number of decimal places. By default this function rounds the nearest integer, and returns a whole number with no decimal places:
+my_int_1 = 4.798
+my_int_2 = 4.256
+rounded_int_1 = round(my_int_1) # This is the default that will round it to the nearest whole number
+rounded_int_2 = round(my_int_2, 1) # the 1 after indicates how many deciamal places you actually want to keep when rounding 
+print(rounded_int_1) # 5
+print(rounded_int_2) # 4.3
+
+# abs() : returns the absolute value of a number 
+num = -15
+absolute_value = abs(num)
+print(absolute_value) # 15
+
+# pow() : raises a number to the nearest power of another or performs modular exponentiation.
+result_1 = pow(2, 3) # Equivalent to 2 ** 3
+print(result_1) # 8
+result_2 = pow(2, 3, 5) # (2 ** 3) % 5
+print(result_2) # 3
