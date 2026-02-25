c5f8216294a2389a89458fe9206b5acebc7ad4f1
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-02-24 20:49:01 -0600

    first 2 coding programs in Code Camp

---
+++ CodePath-101.py
@@ -0,0 +1,14 @@
+# Problem Set version 1
+# Problem 1: hello World!
+def hello_world(): 
+    print("Hello world!")
+
+hello_world() # Prints 'Hello world!'
+
+# Problem 2: Today's Mood
+def todays_mood():
+    mood = "😎"
+    print("Today's mood: " +  mood)
+
+todays_mood()
+
