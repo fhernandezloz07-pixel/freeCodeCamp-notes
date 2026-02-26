7f00423264d9347d191016a20027fe90324ab11a
Your Name <fhernandezloz07-pixel@users.noreply.github.com>
2026-02-25 22:23:36 -0600

    Worked through problem 3 and 4 from the Code Path in class assignemnts. Strugglig a bit but getting it!

--- CodePath-101.py
+++ CodePath-101.py
@@ -12,3 +12,21 @@ def todays_mood():
 
 todays_mood()
 
+# Problem 3: Lunch Menus
+def print_menu(menu):
+    print("Lunch menu to day is: " + menu)
+
+menu = "🍕"
+print_menu(menu) # Prints the menu item to console
+# The variable listed between the () of a function definition is known as a parameter. 
+
+# Problem 4: Sum of Two Integers
+def sum(a, b): # here we are sort of declaring sum() to know what it will do for all cases
+    return a + b
+first = sum(13, 27) # use sum() to calculate the sum of 13 and 27
+result = sum(first, first) # use sum() to double the calculated sum
+
+print(result)# Print the result to the console
+
+# Problem 5
+
